105135bb3a5dfaf5048461cb4dffc8f5960bdeb6
Simon Dietz <h.s.dietz@web.de>
2022-11-08 23:13:56 +0100

    Working on generic graphics.

---
+++ PixelUi2022/src/ui/Graphics2.java
@@ -0,0 +1,27 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.geom.Area;
+import java.awt.image.BufferedImage;
+
+public interface Graphics2 {
+	
+	void setColor(Color color);
+	
+	void drawLine(int x0, int y0, int x1, int y1);
+	
+	void drawRect(int x, int y, int width, int height);
+	
+	void fillRect(int x, int y, int width, int height);
+	
+	void drawRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight);
+	
+	void translate(int x, int y);
+	
+	void drawString(String text, int x, int y);
+	
+	void drawImage(BufferedImage image, int x, int y);
+	
+	void fillArea(Area area);
+
+}
